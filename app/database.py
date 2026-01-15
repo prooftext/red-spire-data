1e3fa9ae3ec9ca1aedca0e9b9b61062fe8109ecf
Avi Kessner <akessner@gmail.com>
2026-01-15 13:25:15 +0200

    server is up and running

--- app/database.py
+++ app/database.py
@@ -1,11 +1,12 @@
 import psycopg
+from psycopg_pool import AsyncConnectionPool
 from app.config import settings
 
 pool = None
 
 async def init_db():
     global pool
-    pool = psycopg.AsyncConnectionPool(settings.DATABASE_URL, min_size=2, max_size=10)
+    pool = AsyncConnectionPool(settings.DATABASE_URL, min_size=2, max_size=10)
 
 async def close_db():
     global pool
